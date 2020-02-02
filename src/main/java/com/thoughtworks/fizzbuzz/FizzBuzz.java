7d678c0796aca613b37dd5dec9e1588c67a9538c
TKJohn <fzwjohnfan@gmail.com>
2020-02-03 00:07:55 +0800

    Return Fizz, Buzz or Whizz when multiply by 3, 5 or 7.

--- src/main/java/com/thoughtworks/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/thoughtworks/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.thoughtworks.fizzbuzz;
 
 public class FizzBuzz {
     public static String fizzBuzz(final Integer input) {
+        if (input % 3 == 0) return "Fizz";
+        if (input % 5 == 0) return "Buzz";
+        if (input % 7 == 0) return "Whizz";
         return String.valueOf(input);
     }
 }
--- src/test/java/com/thoughtworks/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/fizzbuzz/FizzBuzzTest.java
@@ -12,4 +12,25 @@ public class FizzBuzzTest {
         String result = FizzBuzz.fizzBuzz(input);
         assertEquals("4", result);
     }
+
+    @Test
+    public void shouldReturnFizzGivenMultiplierOf3() {
+        Integer input = 3;
+        String result = FizzBuzz.fizzBuzz(input);
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void shouldReturnBuzzGivenMultiplierOf5() {
+        Integer input = 5;
+        String result = FizzBuzz.fizzBuzz(input);
+        assertEquals("Buzz", result);
+    }
+
+    @Test
+    public void shouldReturnWhizzGivenMultiplierOf7() {
+        Integer input = 7;
+        String result = FizzBuzz.fizzBuzz(input);
+        assertEquals("Whizz", result);
+    }
 }
